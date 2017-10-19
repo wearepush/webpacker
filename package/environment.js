5e41b36a58c7067107e419e77d219eb5ba3e370f
TimurZ <timurztv@gmail.com>
2017-10-19 19:54:19 +0300

    [feat] change webpacker extensions

--- package/environment.js
+++ package/environment.js
@@ -35,7 +35,8 @@ function getExtensionsGlob() {
   if (!extensions.length) {
     throw new Error('You must configure at least one extension to compile in webpacker.yml')
   }
-  return extensions.length === 1 ? `**/${extensions[0]}` : `**/*{${extensions.join(',')}}`
+  // return extensions.length === 1 ? `**/${extensions[0]}` : `**/*{${extensions.join(',')}}`
+  return `*{${extensions.join(',')}}*`;
 }
 
 function getEntryObject() {
